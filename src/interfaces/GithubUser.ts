14d1de87e812263b0916701db1e10dd47c387e7e
artartur <arturgfs@hotmail.com>
2026-05-09 05:57:49 -0300

    feat(interfaces): create GithubUser

---
+++ src/interfaces/GithubUser.ts
@@ -0,0 +1,13 @@
+export interface GitHubUser {
+  avatar_url: string;
+  bio: string | null;
+  blog: string | null;
+  company: string | null;
+  created_at: string;
+  followers: number;
+  following: number;
+  location: string | null;
+  login: string;
+  name: string;
+  public_repos: number;
+}
