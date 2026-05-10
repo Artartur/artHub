0cb6bdc2a8639d455398665b3a05a3bcc181872b
artartur <arturgfs@hotmail.com>
2026-05-10 07:55:20 -0300

    feat(hooks): create useUser hook to handle api results by params

---
+++ src/hooks/useUser.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import type { GitHubUser } from "../interfaces/GithubUser";
+import getGithubErrorMessage from "../utils/githubErrorsResponse";
+
+export function useUser(
+  username: string | undefined,
+  initialUser?: GitHubUser,
+) {
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(!initialUser);
+  const [user, setUser] = useState<GitHubUser | null>(initialUser ?? null);
+
+  useEffect(() => {
+    if (initialUser || !username) return;
+
+    axios
+      .get<GitHubUser>(`${import.meta.env.VITE_API_URL}/users/${username}`)
+      .then((res) => setUser(res.data))
+      .catch((err) => setError(getGithubErrorMessage(err)))
+      .finally(() => setLoading(false));
+  }, [username, initialUser]);
+
+  return { user, loading, error };
+}
