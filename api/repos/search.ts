88b6710a525ef4e112af5f4e56668d1c31f7be23
artartur <arturgfs@hotmail.com>
2026-05-10 08:15:27 -0300

    refactor(api): get api url by env

--- api/repos/search.ts
+++ api/repos/search.ts
@@ -1,19 +1,19 @@
 import axios from "axios";
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
+const API_URL = process.env.VITE_API_URL as string;
+
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   const { order, page, per_page, q, sort } = req.query;
 
   try {
-    const { data } = await axios.get(
-      "https://api.github.com/search/repositories",
-      {
-        params: { q, sort, order, per_page, page },
-        headers: {
-          Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
-        },
+    const { data } = await axios.get(`${API_URL}/search/repositories`, {
+      params: { q, sort, order, per_page, page },
+      headers: {
+        Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
       },
-    );
+    });
+
     res.json(data);
   } catch (err) {
     if (axios.isAxiosError(err)) {
--- api/users/[username].ts
+++ api/users/[username].ts
@@ -1,18 +1,18 @@
 import axios from "axios";
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
+const API_URL = process.env.VITE_API_URL as string;
+
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   const { username } = req.query;
 
   try {
-    const { data } = await axios.get(
-      `https://api.github.com/users/${username}`,
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
-        },
+    const { data } = await axios.get(`${API_URL}/users/${username}`, {
+      headers: {
+        Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
       },
-    );
+    });
+
     res.json(data);
   } catch (err) {
     if (axios.isAxiosError(err)) {
